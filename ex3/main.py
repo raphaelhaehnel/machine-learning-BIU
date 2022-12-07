7ba99355befe61f307112fa3347a8f28440cc041
Raphael <raphael.haehnel@gmail.com>
2022-12-07 08:05:51 +0200

    y vector not working

--- ex3/main.py
+++ ex3/main.py
@@ -12,10 +12,9 @@ def generate_data(n, c, dim):
     means = 12*(np.random.rand(c, dim) - 0.5)
     means = means.repeat(n, axis=0)
 
-    data = means + np.random.randn(n*c, dim)
-    col = np.zeros((n*c, 1))
-    data = np.append(data, col, axis=1)
-    return data
+    X_train = means + np.random.randn(n*c, dim)
+    y_train = np.zeros(n*c)
+    return X_train, y_train
 
 
 def generate_means(k, dim):
@@ -24,9 +23,9 @@ def generate_means(k, dim):
     return np.append(means, label, axis=1)
 
 
-def show_plot(data, mu, i, save, dim):
+def show_plot(X_train, y_train, mu, i, save, dim):
     if dim == 2:
-        plt.scatter(x=data[:, 0], y=data[:, 1], c=data[:, 2], s=20)
+        plt.scatter(x=X_train[:, 0], y=X_train[:, 1], c=y_train, s=20)
         plt.scatter(x=mu[:, 0], y=mu[:, 1], c=mu[:, 2],
                     marker="*", edgecolors="black", s=100)
         plt.xlim([-10, 10])
@@ -35,7 +34,8 @@ def show_plot(data, mu, i, save, dim):
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
 
-        ax.scatter(data[:, 0], data[:, 1], data[:, 2], c=data[:, 3], s=20)
+        ax.scatter(X_train[:, 0], X_train[:, 1],
+                   X_train[:, 2], c=y_train, s=20)
         ax.scatter(mu[:, 0], mu[:, 1], mu[:, 2], c=mu[:, 3],
                    marker="*", edgecolors="black", s=100)
 
@@ -48,8 +48,8 @@ def show_plot(data, mu, i, save, dim):
         plt.show()
 
 
-def duplicate_mu(data, mu, dim):
-    n_data = data.shape[0]
+def duplicate_mu(X_train, mu, dim):
+    n_data = X_train.shape[0]
     n_mu = mu.shape[0]
 
     # Duplicate the values of mu along the vertical axis
@@ -60,47 +60,46 @@ def duplicate_mu(data, mu, dim):
     return np.vsplit(mu_v, n_mu)
 
 
-def assignment(data, mu, dim):
+def assignment(X_train, y_train, mu, dim):
 
-    mu_duplicated = duplicate_mu(data, mu, dim)
-    x = data[:, :dim]
-    result = np.zeros((1, data.shape[0]))
+    mu_duplicated = duplicate_mu(X_train, mu, dim)
+    result = np.zeros((1, X_train.shape[0]))
 
     for i in range(mu.shape[0]):
-        diff = x-mu_duplicated[i]
+        diff = X_train-mu_duplicated[i]
         result = np.vstack([result, np.apply_along_axis(
             lambda a: a[0]**2+a[1]**2, 1, diff)])
     result = result[1:]
     arg_min = np.argmin(result, 0)
 
-    data[:, dim] = arg_min
+    y_train = arg_min
 
-    return data
+    return y_train
 
 
-def helper_sum(data, k, dim):
+def helper_sum(X_train, y_train, k, dim):
     result = np.zeros((k, dim))
     if dim == 2:
-        result[int(data[dim])] = [data[0], data[1]]
+        result[y_train] = [X_train[0], X_train[1]]
     if dim == 3:
-        result[int(data[dim])] = [data[0], data[1], data[2]]
+        result[y_train] = [X_train[0], X_train[1], X_train[2]]
 
     return result
 
 
-def get_linked_data(data, k, dim):
+def get_linked_data(y_train, k, dim):
 
     sum_data_linked = np.zeros((1, k))
-    for row in data:
-        sum_data_linked[0, int(row[dim])] += 1
+    for value in y_train:
+        sum_data_linked[0, int(value)] += 1
 
     return sum_data_linked
 
 
-def centroid_update(data, k, dim):
-    linked_data = get_linked_data(data, k, dim)
+def centroid_update(X_train, y_train, k, dim):
+    linked_data = get_linked_data(y_train, k, dim)
     new_mu = np.apply_along_axis(
-        functools.partial(helper_sum, k=k, dim=dim), 1, data)  # dim is not supposed to be an array
+        functools.partial(helper_sum, y_train=y_train, k=k, dim=dim), 1, X_train)
 
     new_mu = np.sum(new_mu, axis=0)
     new_mu = np.divide(new_mu, linked_data.repeat(dim, 0).T, out=np.zeros_like(
@@ -111,17 +110,17 @@ def centroid_update(data, k, dim):
     return new_mu
 
 
-def run_clustering(data, k, mu, dim):
+def run_clustering(X_train, y_train, k, mu, dim):
 
     for i in range(LIMIT_ITER):
-        data = assignment(data, mu, dim)
-        new_mu = centroid_update(data, k, dim)
+        y_train = assignment(X_train, y_train, mu, dim)
+        new_mu = centroid_update(X_train, y_train, k, dim)
         if np.array_equal(new_mu, mu):
             break
         mu = new_mu
-        show_plot(data, mu, i, save=True, dim=dim)
+        show_plot(X_train, y_train, mu, i, save=True, dim=dim)
 
-    return data
+    return y_train
 
 
 def cost_function():
@@ -164,11 +163,11 @@ if __name__ == "__main__":
     if os.path.exists('./output'):
         shutil.rmtree('./output')
 
-    X_train, y_train, X_test, y_test = retrive_data()
+    # X_train, y_train, X_test, y_test = retrive_data()
 
-    data = generate_data(n, c, dim)
+    X_train, y_train = generate_data(n, c, dim)
     mu = generate_means(k, dim)
 
-    output = run_clustering(data, k, mu, dim)
+    y_output = run_clustering(X_train, y_train, k, mu, dim)
 
     print("EOF")
